47ef3d64c4b4024697c490ea19586d979af6314c
JUB_Yoush <yoush1337@gmail.com>
2026-01-14 14:16:48 -0500

    first commit

---
+++ impls/me/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static string Read(string val)
+    {
+        return val;
+    }
+
+    static string Eval(string val)
+    {
+        return val;
+    }
+
+    static void Print(string val)
+    {
+        Console.WriteLine(val);
+    }
+
+    static void REP() { }
+}
